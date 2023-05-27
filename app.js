9ee00428a9afdf83396bc0fdc2bb8a711e287dae
1M-GA <athrv.gajbhiye.xxii@gmail.com>
2023-05-27 16:37:59 +0530

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const http = require('http');
 
+let username = '';
+
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+      `<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form>`
     );
-    res.write('</html>');
+
+    if (username) {
+      res.write(`<p>Username: ${username}</p>`);
+    }
+    
+    res.write('</body></html>');
     return res.end();
   }
   if (url === '/users') {
@@ -25,12 +32,15 @@ const server = http.createServer((req, res) => {
     });
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody.split('=')[1]);
+      username = parsedBody.split('=')[1];
+      console.log(username);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      res.end();
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    res.end();
   }
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
